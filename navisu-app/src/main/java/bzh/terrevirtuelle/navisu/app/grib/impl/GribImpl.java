38c7c9d2117652500bf5d2524d98accfd1372daa
Jordan <jordan.mens@gmail.com>
2013-11-24 14:37:02 +0100

    Define grib services

--- navisu-app/src/main/java/bzh/terrevirtuelle/navisu/app/grib/impl/GribImpl.java
+++ navisu-app/src/main/java/bzh/terrevirtuelle/navisu/app/grib/impl/GribImpl.java
@@ -5,11 +5,12 @@ import bzh.terrevirtuelle.navisu.app.grib.Grib;
 import bzh.terrevirtuelle.navisu.app.grib.GribConstants;
 import bzh.terrevirtuelle.navisu.app.grib.GribServices;
 import org.capcaval.c3.component.ComponentState;
-import ucar.ma2.Array;
+import ucar.nc2.dt.GridDatatype;
 import ucar.nc2.dt.grid.GeoGrid;
 import ucar.nc2.dt.grid.GridDataset;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -21,12 +22,6 @@ public class GribImpl implements Grib, GribServices, ComponentState {
     protected Logger LOGGER = Logger.getLogger(GribImpl.class.getName());
 
     protected Driver driver;
-
-    protected GeoGrid uGrid;
-
-    protected GeoGrid vGrid;
-
-    protected GeoGrid pressionGrid;
 
     @Override
     public void componentInitiated() {
@@ -44,7 +39,6 @@ public class GribImpl implements Grib, GribServices, ComponentState {
             public void open(String... files) {
                 for(String file : files) {
                     LOGGER.info("Opening " + file + " ...");
-                    loadFile(files[0]); //TODO load all files !
                 }
             }
 
@@ -73,43 +67,11 @@ public class GribImpl implements Grib, GribServices, ComponentState {
 
     @Override
     public void loadFile(String path) {
-        GridDataset gd = null;
-        try {
-            gd = GridDataset.open(path);
-        } catch (IOException e) {
-            e.printStackTrace(); //TODO Make custom exception for drivers
-        }
 
-        if(null == gd.findGridByName(GribConstants.PRESSURE_GRID_NAME) ||
-                null == gd.findGridByName(GribConstants.U_GRID_NAME) ||
-                null == gd.findGridByName(GribConstants.V_GRID_NAME)) {
-
-            return; //TODO Make custom Exception here
-        } else {
-
-            this.pressionGrid = gd.findGridByName(GribConstants.PRESSURE_GRID_NAME);
-            this.uGrid = gd.findGridByName(GribConstants.U_GRID_NAME);
-            this.vGrid = gd.findGridByName(GribConstants.V_GRID_NAME);
-        }
     }
 
     @Override
     public double[] getVelocityInMPSAtPoint(double latitude, double longitude) {
-
-        double[] velocity = new double[2];
-
-        Array uArray;
-        Array vArray;
-
-        try {
-            uArray = this.uGrid.readYXData(0,0);
-            vArray = this.vGrid.readYXData(0,0);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        int[] uVelocityIndex = this.uGrid.getCoordinateSystem().findXYindexFromLatLon(latitude, longitude, null);
-        int[] vVelocityIndex = this.vGrid.getCoordinateSystem().findXYindexFromCoord(latitude, longitude, null);
 
         return null;
     }
