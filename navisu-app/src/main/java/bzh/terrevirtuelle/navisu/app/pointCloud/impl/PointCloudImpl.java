966b8231ad9a9897b1ba9ee8521b73c5660d1615
Neymak <julienlandrein@gmail.com>
2013-11-26 23:44:53 +0100

    Component initialization

--- navisu-app/src/main/java/bzh/terrevirtuelle/navisu/app/pointCloud/impl/PointCloudImpl.java
+++ navisu-app/src/main/java/bzh/terrevirtuelle/navisu/app/pointCloud/impl/PointCloudImpl.java
@@ -5,15 +5,49 @@ import bzh.terrevirtuelle.navisu.app.pointcloud.PointCloud;
 import bzh.terrevirtuelle.navisu.app.pointcloud.PointCloudServices;
 import org.capcaval.c3.component.ComponentState;
 
+import java.util.logging.Logger;
+
 /**
  * User: julien
  * Date: 26/11/13
  */
 public class PointCloudImpl implements PointCloud, PointCloudServices, ComponentState {
 
+    protected Driver driver;
+
+    protected Logger LOGGER = Logger.getLogger(PointCloudImpl.class.getName());
+
+
     @Override
     public void componentInitiated() {
+        this.driver = new Driver() {
 
+            private static final String NAME = "PointCloud";
+            private static final String EXTENSION = ".las";
+
+            @Override
+            public boolean canOpen(String file) {
+                return file.toLowerCase().endsWith(EXTENSION);
+            }
+
+            @Override
+            public void open(String... files) {
+                for(String file : files) {
+                    LOGGER.info("Opening " + file + " ...");
+                    loadFile(files[0]); //Todo Make stuff for all files
+                }
+            }
+
+            @Override
+            public String getName() {
+                return NAME;
+            }
+
+            @Override
+            public String[] getExtensions() {
+                return new String[] { "*" + EXTENSION };
+            }
+        };
     }
 
     @Override
@@ -24,7 +58,7 @@ public class PointCloudImpl implements PointCloud, PointCloudServices, Component
 
     @Override
     public Driver getDriver() {
-        return null;
+        return this.driver;
     }
 
     @Override
