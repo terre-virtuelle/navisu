8bbf49f2b3178ababec3b3491bc2140aaed04e49
Serge <sergemorvan29@gmail.com>
2014-01-23 20:48:22 +0100

    Création du module navisuèship modèle de Ship.

--- navisu-ship/src/main/java/bzh/terrevirtuelle/navisu/ship/Category.java
+++ navisu-ship/src/main/java/bzh/terrevirtuelle/navisu/ship/Category.java
@@ -10,10 +10,10 @@ public class Category {
     public static final Map<Integer, String> TYPE = Collections.unmodifiableMap(new HashMap<Integer, String>() {
         {
             put(20, " Wing in ground (WIG), all ships of this type");
-            put(21, " Wing in ground (WIG), Hazardous catagory A");
-            put(22, " Wing in ground (WIG), Hazardous catagory B");
-            put(23, " Wing in ground (WIG), Hazardous catagory C");
-            put(24, " Wing in ground (WIG), Hazardous catagory D");
+            put(21, " Wing in ground (WIG), Hazardous category A");
+            put(22, " Wing in ground (WIG), Hazardous category B");
+            put(23, " Wing in ground (WIG), Hazardous category C");
+            put(24, " Wing in ground (WIG), Hazardous category D");
             put(25, " Wing in ground (WIG), Reserved for future use");
             put(26, " Wing in ground (WIG), Reserved for future use");
             put(27, " Wing in ground (WIG), Reserved for future use");
@@ -30,10 +30,10 @@ public class Category {
             put(38, " reserved");
             put(39, " reserved");
             put(40, " High speed craft (HSC), all ships of this type");
-            put(41, " High speed craft (HSC), Hazardous catagory A");
-            put(42, " High speed craft (HSC), Hazardous catagory B");
-            put(43, " High speed craft (HSC), Hazardous catagory C");
-            put(44, " High speed craft (HSC), Hazardous catagory D");
+            put(41, " High speed craft (HSC), Hazardous category A");
+            put(42, " High speed craft (HSC), Hazardous category B");
+            put(43, " High speed craft (HSC), Hazardous category C");
+            put(44, " High speed craft (HSC), Hazardous category D");
             put(45, " High speed craft (HSC), Reserved for future use");
             put(46, " High speed craft (HSC), Reserved for future use");
             put(47, " High speed craft (HSC), Reserved for future use");
@@ -50,40 +50,40 @@ public class Category {
             put(58, " medical transpor");
             put(59, " ship according to RR Resolution No. 18");
             put(60, " passenger, all ships of this type");
-            put(61, " passenger, Hazardous catagory A");
-            put(62, " passenger, Hazardous catagory B");
-            put(63, " passenger, Hazardous catagory C");
-            put(64, " passenger, Hazardous catagory D");
+            put(61, " passenger, Hazardous category A");
+            put(62, " passenger, Hazardous category B");
+            put(63, " passenger, Hazardous category C");
+            put(64, " passenger, Hazardous category D");
             put(65, " passenger, Reserved for future use");
             put(66, " passenger, Reserved for future use");
             put(67, " passenger, Reserved for future use");
             put(68, " passenger, Reserved for future use");
             put(69, " passenger, No additional information");
             put(70, " cargo, all ships of this type");
-            put(71, " cargo, Hazardous catagory A");
-            put(72, " cargo, Hazardous catagory B");
-            put(73, " cargo, Hazardous catagory C");
-            put(74, " cargo, Hazardous catagory D");
+            put(71, " cargo, Hazardous category A");
+            put(72, " cargo, Hazardous category B");
+            put(73, " cargo, Hazardous category C");
+            put(74, " cargo, Hazardous category D");
             put(75, " cargo, Reserved for future use");
             put(76, " cargo, Reserved for future use");
             put(77, " cargo, Reserved for future use");
             put(78, " cargo, Reserved for future use");
             put(79, " cargo, No additional information");
             put(80, " tanker, all ships of this type");
-            put(81, " tanker, Hazardous catagory A");
-            put(82, " tanker, Hazardous catagory B");
-            put(83, " tanker, Hazardous catagory C");
-            put(84, " tanker, Hazardous catagory D");
+            put(81, " tanker, Hazardous category A");
+            put(82, " tanker, Hazardous category B");
+            put(83, " tanker, Hazardous category C");
+            put(84, " tanker, Hazardous category D");
             put(85, " tanker, Reserved for future use");
             put(86, " tanker, Reserved for future use");
             put(87, " tanker, Reserved for future use");
             put(88, " tanker, Reserved for future use");
             put(89, " tanker, No additional information");
             put(90, " other type, all ships of this type");
-            put(91, " other type, Hazardous catagory A");
-            put(92, " other type, Hazardous catagory B");
-            put(93, " other type, Hazardous catagory C");
-            put(94, " other type, Hazardous catagory D");
+            put(91, " other type, Hazardous  A");
+            put(92, " other type, Hazardous  B");
+            put(93, " other type, Hazardous  C");
+            put(94, " other type, Hazardous  D");
             put(95, " other type, Reserved for future use");
             put(96, " other type, Reserved for future use");
             put(97, " other type, Reserved for future use");
@@ -104,8 +104,5 @@ public class Category {
         return buffer.toString();
     }
 
-    public static void main(String[] args) {
-        Category category = new Category();
-        System.out.println(category);
-    }
+    
 }
