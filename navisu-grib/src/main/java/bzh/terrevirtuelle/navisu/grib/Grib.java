ff817f56b20051adb1b98558e7b34a891e7c93df
Serge <sergemorvan29@gmail.com>
2014-01-24 18:04:54 +0100

    Création du module grib

---
+++ navisu-grib/src/main/java/bzh/terrevirtuelle/navisu/grib/Grib.java
@@ -0,0 +1,10 @@
+package bzh.terrevirtuelle.navisu.grib;
+
+import org.capcaval.c3.component.Component;
+
+/**
+ * User: jordan
+ * Date: 23/11/2013
+ */
+public interface Grib extends Component {
+}
