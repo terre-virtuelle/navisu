31e45426b391c15f02e861aaf40dc7b976b78301
Serge <sergemorvan29@gmail.com>
2014-02-26 15:28:09 +0100

    Delete MObject. OObject inherit TObject.

--- navisu-core/src/main/java/bzh/terrevirtuelle/navisu/core/model/tobject/OObject.java
+++ navisu-core/src/main/java/bzh/terrevirtuelle/navisu/core/model/tobject/OObject.java
@@ -13,22 +13,33 @@ import bzh.terrevirtuelle.navisu.geodesy.Orientation;
  * @author Thibault Pensec <thibault.pensec at gmail.com>
  * @author Jordan Mens <jordan.mens at gmail.com>
  */
-public interface OObject extends ICloneable {
-
-    int getID();
+public interface OObject extends TObject {
 
     Orientation getOrientation();
 
     void setOrientation(Orientation orientation);
 
-    public static OObject newBasicOObject(final int id, final double orientation) {
+    public static OObject newBasicOObject(final int id,
+            final double lat, final double lon,
+            final double orientation) {
         return new OObject() {
-
+            
+            Location loc = Location.factory.newLocation(lat, lon);
             Orientation angle = Orientation.factory.newOrientation(orientation);
 
             @Override
             public int getID() {
                 return id;
+            }
+
+            @Override
+            public Location getLocation() {
+                return loc;
+            }
+
+            @Override
+            public void setLocation(Location location) {
+                loc = location;
             }
 
             @Override
@@ -43,7 +54,9 @@ public interface OObject extends ICloneable {
 
             @Override
             public Object getClone() {
-                return OObject.newBasicOObject(id, angle.getOrientationDegree());
+                return OObject.newBasicOObject(id,
+                        loc.getLatitudeDegree(), loc.getLongitudeDegree(),
+                        angle.getOrientationDegree());
             }
         };
     }
