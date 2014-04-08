ac62a356859f60de015d091988d1f6afa1f4c76d
Mhyrdin <contact@mhyrdin.fr>
2014-04-08 16:01:27 +0200

    maj icones et dock

--- navisu-app/src/main/java/bzh/terrevirtuelle/navisu/app/guiagent/impl/GuiAgentImpl.java
+++ navisu-app/src/main/java/bzh/terrevirtuelle/navisu/app/guiagent/impl/GuiAgentImpl.java
@@ -185,8 +185,7 @@ public class GuiAgentImpl
         dock.setLayoutY(30.0);
         dock.setOrientation(Orientation.HORIZONTAL);
         StackPane.setAlignment(groupDock, Pos.BOTTOM_CENTER);
-        //Animation downAnimation = AnimationFactory.newTranslateAnimation(groupDock, 200, 300);
-        Animation downAnimation = AnimationFactory.newTranslateAnimation(groupDock, 200, 280);
+        Animation downAnimation = AnimationFactory.newTranslateAnimation(groupDock, 200, 300);
         Animation upAnimation = AnimationFactory.newTranslateAnimation(groupDock, 200, 0);
         scene.setOnKeyPressed((KeyEvent ke) -> {
             if (ke.getCode() == KeyCode.DOWN) {
