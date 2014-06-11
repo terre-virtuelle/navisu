a576ec9ca502ab695018014c3567253ba17e9c69
Serge <sergemorvan29@gmail.com>
2014-06-11 18:00:42 +0200

    Refactoring Beacon modles

--- navisu-domain/src/main/java/bzh/terrevirtuelle/navisu/domain/charts/vector/s57/attributes/CategoryOfLateralMark.java
+++ navisu-domain/src/main/java/bzh/terrevirtuelle/navisu/domain/charts/vector/s57/attributes/CategoryOfLateralMark.java
@@ -5,21 +5,21 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-public class CategoryOfCardinalMark {
+public class CategoryOfLateralMark {
 
     public static final Map<String, String> ATT = Collections.unmodifiableMap(new HashMap<String, String>() {
         {
-            put("1", "North");
-            put("2", "East");
-            put("3", "South");
-            put("4", "West");
+            put("1", "Port hand");
+            put("2", "Starboard hand");
+            put("3", "Port hand preferred channel");
+            put("4", "Starboard hand preferred channel");
         }
     });
 
     @Override
     public String toString() {
         StringBuilder buffer = new StringBuilder();
-        Set<Map.Entry<String, String>> entries = CategoryOfCardinalMark.ATT.entrySet();
+        Set<Map.Entry<String, String>> entries = CategoryOfLateralMark.ATT.entrySet();
         buffer.append("[");
         entries.stream().forEach((e) -> {
             buffer.append("[").append(e.getKey()).append(", ").append(e.getValue()).append("]");
--- navisu-domain/src/main/java/bzh/terrevirtuelle/navisu/domain/charts/vector/s57/geo/BeaconLateral.java
+++ navisu-domain/src/main/java/bzh/terrevirtuelle/navisu/domain/charts/vector/s57/geo/BeaconLateral.java
@@ -1,10 +1,9 @@
 package bzh.terrevirtuelle.navisu.domain.charts.vector.s57.geo;
 
-import bzh.terrevirtuelle.navisu.domain.charts.vector.s57.Geo;
 import java.io.Serializable;
 
 
-public class BeaconLateral extends Geo
+public class BeaconLateral extends Location
 implements Serializable
 {
 
