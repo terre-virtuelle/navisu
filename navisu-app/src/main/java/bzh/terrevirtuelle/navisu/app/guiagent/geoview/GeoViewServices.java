2eac52d0ad8fb2b2923fc93a4d49718daab994d9
Serge <sergemorvan29@gmail.com>
2014-06-11 18:01:01 +0200

    Nothing

--- navisu-app/src/main/java/bzh/terrevirtuelle/navisu/app/guiagent/geoview/GeoViewServices.java
+++ navisu-app/src/main/java/bzh/terrevirtuelle/navisu/app/guiagent/geoview/GeoViewServices.java
@@ -3,7 +3,6 @@ package bzh.terrevirtuelle.navisu.app.guiagent.geoview;
 import bzh.terrevirtuelle.navisu.app.guiagent.geoview.gobject.GObjectCUDProcessor;
 import bzh.terrevirtuelle.navisu.core.view.display.Display;
 import bzh.terrevirtuelle.navisu.core.view.geoview.layer.LayerManager;
-import bzh.terrevirtuelle.navisu.core.view.geoview.worldwind.GeoWorldWindView;
 import gov.nasa.worldwind.layers.Layer;
 import javafx.scene.Node;
 import org.capcaval.c3.component.ComponentService;
