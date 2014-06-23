16e11d449efa3a89ad0df43cec53dd7b5188793f
Serge <sergemorvan29@gmail.com>
2014-06-23 17:53:27 +0200

    Extension Wreck and obstruction models

--- navisu-domain/src/main/java/bzh/terrevirtuelle/navisu/domain/charts/vector/s57/geo/Wreck.java
+++ navisu-domain/src/main/java/bzh/terrevirtuelle/navisu/domain/charts/vector/s57/geo/Wreck.java
@@ -1,10 +1,9 @@
 package bzh.terrevirtuelle.navisu.domain.charts.vector.s57.geo;
 
-import bzh.terrevirtuelle.navisu.domain.charts.vector.s57.Geo;
 import java.io.Serializable;
 
 
-public class Wreck extends Geo
+public class Wreck extends Location
 implements Serializable
 {
 
