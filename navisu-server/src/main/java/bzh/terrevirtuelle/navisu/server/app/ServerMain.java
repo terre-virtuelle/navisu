348834a4292ccdc6f645977b6b43dd031dd4ac65
Serge <sergemorvan29@gmail.com>
2014-07-13 10:37:03 +0200

    delete JavaFX for stop server

--- navisu-server/src/main/java/bzh/terrevirtuelle/navisu/server/app/ServerMain.java
+++ navisu-server/src/main/java/bzh/terrevirtuelle/navisu/server/app/ServerMain.java
@@ -1,32 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package bzh.terrevirtuelle.navisu.server.app;
 
 import bzh.terrevirtuelle.navisu.server.DataServerServices;
 import bzh.terrevirtuelle.navisu.server.impl.vertx.DataServerImpl;
-import java.io.FileInputStream;
 import java.util.logging.Level;
-import java.util.logging.LogManager;
 import java.util.logging.Logger;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 import org.capcaval.c3.componentmanager.ComponentManager;
 
 /**
- * Created with IntelliJ IDEA. User: Serge Morvan Date: 21/12/13 Time: 18:18
+ *
+ * @author Serge Morvan
+ * @date 10 juil. 2014 NaVisu project
  */
-public class ServerMain extends Application {
+public class ServerMain {
 
     private static final Logger LOGGER = Logger.getLogger(ServerMain.class.getName());
 
-    @Override
-    public void start(Stage stage) throws Exception {
+    public static void main(String[] args) {
         final ComponentManager componentManager = ComponentManager.componentManager;
-
         // deploy components
         LOGGER.log(Level.INFO, "\n Start", componentManager.startApplication(
                 DataServerImpl.class
@@ -34,30 +29,11 @@ public class ServerMain extends Application {
         DataServerServices nmeaServerServices = componentManager.getComponentService(DataServerServices.class);
         // Test avec choix des parametres de comm
         nmeaServerServices.init("localhost", 8080);
-       // nmeaServerServices.openSerialPort("COM4", 4800, 8, 1, 0);
+        // nmeaServerServices.openSerialPort("COM4", 4800, 8, 1, 0);
         nmeaServerServices.openSerialPort("COM5", 4800, 8, 1, 0);
         // Test avec les parametres de comm dans properties/nmea.properties
         //  nmeaServerServices.init();
         //  nmeaServerServices.openSerialPort();
         // nmeaServerServices.openFile();
-
-        Button button = new Button("Stop",
-                new ImageView(new Image(getClass().getResourceAsStream("stop_40x40.png"))));
-        button.setOnAction((ActionEvent event) -> {
-            System.exit(0);
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(button);
-
-        Scene scene = new Scene(root, 300, 250);
-
-        stage.setTitle("ServerMain test");
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public static void main(String[] args) throws Exception {
-        Application.launch();
     }
 }
