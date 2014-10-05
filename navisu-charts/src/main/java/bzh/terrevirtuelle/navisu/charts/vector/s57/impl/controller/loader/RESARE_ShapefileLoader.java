baf1db5c1be000c343a745c2e8903881e4cf6e9a
Serge <sergemorvan29@gmail.com>
2014-10-05 18:00:00 +0200

    RESARE layer OK

--- navisu-charts/src/main/java/bzh/terrevirtuelle/navisu/charts/vector/s57/impl/controller/loader/RESARE_ShapefileLoader.java
+++ navisu-charts/src/main/java/bzh/terrevirtuelle/navisu/charts/vector/s57/impl/controller/loader/RESARE_ShapefileLoader.java
@@ -5,10 +5,15 @@
  */
 package bzh.terrevirtuelle.navisu.charts.vector.s57.impl.controller.loader;
 
+import gov.nasa.worldwind.avlist.AVKey;
 import gov.nasa.worldwind.formats.shapefile.ShapefileRecord;
+import gov.nasa.worldwind.formats.shapefile.ShapefileRecordPolygon;
+import gov.nasa.worldwind.geom.Sector;
+import gov.nasa.worldwind.layers.RenderableLayer;
 import gov.nasa.worldwind.render.BasicShapeAttributes;
 import gov.nasa.worldwind.render.Material;
 import gov.nasa.worldwind.render.ShapeAttributes;
+import gov.nasa.worldwind.render.SurfacePolygons;
 import java.awt.Color;
 //import gov.nasa.worldwindx.examples.util.ShapefileLoader;
 import java.util.Map;
@@ -28,17 +33,45 @@ public class RESARE_ShapefileLoader
 
     @Override
     protected ShapeAttributes createPolygonAttributes(ShapefileRecord record) {
-        this.record = record;
-        entries = record.getAttributes().getEntries();
-        System.out.println("entries PolygonTemplate : " + entries);
+
         ShapeAttributes normalAttributes = new BasicShapeAttributes();
         normalAttributes.setDrawInterior(false);
         normalAttributes.setDrawOutline(true);
-        normalAttributes.setOutlineMaterial(new Material(new Color(197,69,195)));
+        normalAttributes.setOutlineMaterial(new Material(new Color(197, 69, 195)));
         normalAttributes.setOutlineStipplePattern((short) 0xAAAA);
         normalAttributes.setOutlineStippleFactor(5);
-      //  normalAttributes.setOutlineWidth(2d);
+        normalAttributes.setEnableLighting(true);
+
         return normalAttributes;
+    }
+
+    @Override
+    protected void createPolygon(ShapefileRecord record, ShapeAttributes attrs, RenderableLayer layer) {
+        SurfacePolygons shape = new SurfacePolygons(
+                Sector.fromDegrees(((ShapefileRecordPolygon) record).getBoundingRectangle()),
+                record.getCompoundPointBuffer());
+        shape.setAttributes(attrs);
+        shape.setWindingRule(AVKey.CLOCKWISE);
+        shape.setPolygonRingGroups(new int[]{0});
+        shape.setPolygonRingGroups(new int[]{0});
+
+        ShapeAttributes highlightAttributes = new BasicShapeAttributes(attrs);
+        highlightAttributes.setOutlineOpacity(1);
+        highlightAttributes.setDrawInterior(true);
+        highlightAttributes.setInteriorMaterial(new Material(new Color(197, 69, 195)));
+        shape.setHighlightAttributes(highlightAttributes);
+
+        this.record = record;
+        entries = record.getAttributes().getEntries();
+
+        entries.stream().forEach((e) -> {
+            if (e.getKey().equals("INFORM")) {
+                shape.setValue(AVKey.DISPLAY_NAME, (String) e.getValue());
+            }
+        });
+
+       // shape.setValue(AVKey.DISPLAY_NAME, "Has a hole\nRotated -170\u00b0");
+        layer.addRenderable(shape);
     }
 
     @Override
