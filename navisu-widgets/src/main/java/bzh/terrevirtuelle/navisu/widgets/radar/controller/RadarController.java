fbd1de8b4066dc6303d5a7830d8acf9acdebc2ac
Serge <Serge@Serge-PC>
2014-12-29 18:55:02 +0100

    refactor : Radar resources

--- navisu-widgets/src/main/java/bzh/terrevirtuelle/navisu/widgets/radar/controller/RadarController.java
+++ navisu-widgets/src/main/java/bzh/terrevirtuelle/navisu/widgets/radar/controller/RadarController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package bzh.terrevirtuelle.navisu.widgets.radar;
+package bzh.terrevirtuelle.navisu.widgets.radar.controller;
 
 import bzh.terrevirtuelle.navisu.widgets.WidgetController;
 
