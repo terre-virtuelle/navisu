5e71264092bf3d14e05b6b9450fd8401c7d4c0b1
Serge <Serge@Serge-PC>
2014-12-31 13:53:26 +0100

    refactor : tests on JVM

--- gradlew.bat
+++ gradlew.bat
@@ -10,6 +10,7 @@ if "%OS%"=="Windows_NT" setlocal
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS=-Djavafx.embed.singleThread=true
+set JAVA_OPTS=--stackTrace
 
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
@@ -33,7 +34,7 @@ goto fail
 
 :findJavaFromJavaHome
 set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe 
 
 if exist "%JAVA_EXE%" goto init
 
