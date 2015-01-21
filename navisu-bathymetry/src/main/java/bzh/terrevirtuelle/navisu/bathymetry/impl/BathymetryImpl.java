5453da3af002b2fb83e41176ab4d277e001dde03
Serge <Serge@Serge-PC>
2015-01-21 18:34:43 +0100

    create : BathymetryController

--- navisu-bathymetry/src/main/java/bzh/terrevirtuelle/navisu/bathymetry/impl/BathymetryImpl.java
+++ navisu-bathymetry/src/main/java/bzh/terrevirtuelle/navisu/bathymetry/impl/BathymetryImpl.java
@@ -7,52 +7,112 @@ package bzh.terrevirtuelle.navisu.bathymetry.impl;
 
 import bzh.terrevirtuelle.navisu.api.progress.ProgressHandle;
 import bzh.terrevirtuelle.navisu.app.drivers.Driver;
+import bzh.terrevirtuelle.navisu.app.guiagent.geoview.GeoViewServices;
+import bzh.terrevirtuelle.navisu.app.guiagent.layertree.LayerTreeServices;
 import bzh.terrevirtuelle.navisu.bathymetry.Bathymetry;
 import bzh.terrevirtuelle.navisu.bathymetry.BathymetryServices;
+import bzh.terrevirtuelle.navisu.bathymetry.impl.controller.BathymetryController;
+import bzh.terrevirtuelle.navisu.core.view.geoview.layer.GeoLayer;
+import bzh.terrevirtuelle.navisu.core.view.geoview.worldwind.impl.GeoWorldWindViewImpl;
+import gov.nasa.worldwind.WorldWindow;
+import gov.nasa.worldwind.layers.Layer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.capcaval.c3.component.ComponentState;
+import org.capcaval.c3.component.annotation.UsedService;
 
 /**
  *
  * @author Serge
  */
 public class BathymetryImpl
-implements Bathymetry, BathymetryServices, Driver, ComponentState{
+        implements Bathymetry, BathymetryServices, Driver, ComponentState {
+
+    @UsedService
+    GeoViewServices geoViewServices;
+    @UsedService
+    LayerTreeServices layerTreeServices;
+
+    private static final String NAME = "Bathymetry";
+    private static final String EXTENSION_0 = ".glz";
+    protected static final String GROUP = "Bathymetry";
+
+    protected List<Layer> layers;
+    protected static final Logger LOGGER = Logger.getLogger(BathymetryImpl.class.getName());
+    protected WorldWindow wwd;
 
     @Override
     public Driver getDriver() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this;
     }
 
     @Override
     public void openFile(String file) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.open(null, file);
+    }
+
+    @Override
+    public boolean canOpen(String category, String file) {
+        boolean canOpen = false;
+
+        if (category.contains(NAME) && file.toLowerCase().endsWith(EXTENSION_0)) {
+            canOpen = true;
+        }
+        return canOpen;
     }
 
     @Override
     public void open(ProgressHandle progressHandle, String... files) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        for (String file : files) {
+            this.handleOpenFile(progressHandle, file);
+        }
+    }
+
+    protected void handleOpenFile(ProgressHandle pHandle, String fileName) {
+        LOGGER.log(Level.INFO, "Opening {0} ...", fileName);
+        BathymetryController bathymetryController = BathymetryController.getInstance();
+        layers.add(bathymetryController.init(fileName));
+        layers.stream().filter((l) -> (l != null)).map((l) -> {
+            String name = l.getName();
+            //  if (name.contains(NAME)) {
+            l.setPickEnabled(true);
+           // } else {
+            //     l.setPickEnabled(false);
+            //}
+            //  System.out.println("l : "+l);
+            geoViewServices.getLayerManager().insertGeoLayer(GeoLayer.factory.newWorldWindGeoLayer(l));
+            return l;
+        }).forEach((l) -> {
+            layerTreeServices.addGeoLayer(GROUP, GeoLayer.factory.newWorldWindGeoLayer(l));
+        });
+
     }
 
     @Override
     public String getName() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return NAME;
     }
 
     @Override
     public String[] getExtensions() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return new String[]{"*" + EXTENSION_0};
     }
 
     @Override
     public void componentInitiated() {
-           }
+        layerTreeServices.createGroup(GROUP);
+        wwd = GeoWorldWindViewImpl.getWW();
+        layers = new ArrayList<>();
+    }
 
     @Override
     public void componentStarted() {
-      }
+    }
 
     @Override
     public void componentStopped() {
-      }
-    
+    }
+
 }
