e72e4a3807ebbd48e0810d353fbcd8424d900137
serge <serge@serge-XPS-8500>
2015-03-15 19:12:57 +0100

    create : Point2Df, Point2D

--- navisu-domain/src/main/java/bzh/terrevirtuelle/navisu/domain/geometry/Point2D.java
+++ navisu-domain/src/main/java/bzh/terrevirtuelle/navisu/domain/geometry/Point2D.java
@@ -3,82 +3,56 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package bzh.terrevirtuelle.navisu.domain.geometry;
 
 /**
- * @date 12 mars 2015
+ * @date 15 mars 2015
  * @author Serge Morvan
  */
 public class Point2D {
 
-    public float lon;
+    public double lon;
 
-    public float lat;
+    public double lat;
 
-    public float elevation;
+    public double elevation;
 
     public Point2D() {
     }
 
-    public Point2D(float lon, float lat, float elevation) {
+    public Point2D(double lat, double lon) {
+        this.lon = lon;
+        this.lat = lat;
+    }
+
+    public Point2D(double lat, double lon, double elevation) {
         this.lon = lon;
         this.lat = lat;
         this.elevation = elevation;
     }
 
-    /**
-     * Get the value of elevation
-     *
-     * @return the value of elevation
-     */
-    public float getElevation() {
-        return elevation;
-    }
-
-    /**
-     * Set the value of elevation
-     *
-     * @param elevation new value of elevation
-     */
-    public void setElevation(float elevation) {
-        this.elevation = elevation;
-    }
-
-    /**
-     * Get the value of lat
-     *
-     * @return the value of lat
-     */
-    public float getLat() {
-        return lat;
-    }
-
-    /**
-     * Set the value of lat
-     *
-     * @param lat new value of lat
-     */
-    public void setLat(float lat) {
-        this.lat = lat;
-    }
-
-    /**
-     * Get the value of lon
-     *
-     * @return the value of lon
-     */
-    public float getLon() {
+    public double getLon() {
         return lon;
     }
 
-    /**
-     * Set the value of lon
-     *
-     * @param lon new value of lon
-     */
-    public void setLon(float lon) {
+    public void setLon(double lon) {
         this.lon = lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getElevation() {
+        return elevation;
+    }
+
+    public void setElevation(double elevation) {
+        this.elevation = elevation;
     }
 
 }
---
+++ navisu-domain/src/main/java/bzh/terrevirtuelle/navisu/domain/geometry/Point2Df.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bzh.terrevirtuelle.navisu.domain.geometry;
+
+/**
+ * @date 12 mars 2015
+ * @author Serge Morvan
+ */
+public class Point2Df {
+
+    public float lon;
+
+    public float lat;
+
+    public float elevation;
+
+    public Point2Df() {
+    }
+
+    public Point2Df(float lat, float lon, float elevation) {
+        this.lon = lon;
+        this.lat = lat;
+        this.elevation = elevation;
+    }
+
+    /**
+     * Get the value of elevation
+     *
+     * @return the value of elevation
+     */
+    public float getElevation() {
+        return elevation;
+    }
+
+    /**
+     * Set the value of elevation
+     *
+     * @param elevation new value of elevation
+     */
+    public void setElevation(float elevation) {
+        this.elevation = elevation;
+    }
+
+    /**
+     * Get the value of lat
+     *
+     * @return the value of lat
+     */
+    public float getLat() {
+        return lat;
+    }
+
+    /**
+     * Set the value of lat
+     *
+     * @param lat new value of lat
+     */
+    public void setLat(float lat) {
+        this.lat = lat;
+    }
+
+    /**
+     * Get the value of lon
+     *
+     * @return the value of lon
+     */
+    public float getLon() {
+        return lon;
+    }
+
+    /**
+     * Set the value of lon
+     *
+     * @param lon new value of lon
+     */
+    public void setLon(float lon) {
+        this.lon = lon;
+    }
+
+}
