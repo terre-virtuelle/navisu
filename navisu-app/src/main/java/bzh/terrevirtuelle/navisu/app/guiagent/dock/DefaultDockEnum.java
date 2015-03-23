a36d5388b1fcc0a39f4a461e409e71d71c965466
serge <serge@serge-XPS-8500>
2015-03-23 20:49:35 +0100

    refactor : menu

---
+++ navisu-app/src/main/java/bzh/terrevirtuelle/navisu/app/guiagent/dock/DefaultDockEnum.java
@@ -0,0 +1,32 @@
+package bzh.terrevirtuelle.navisu.app.guiagent.menu;
+
+/**
+ * NaVisu
+ *
+ * @author tibus
+ * @date 11/11/2013 21:28
+ */
+public enum DefaultMenuEnum {
+
+    FILE("menu.file"),
+    URL("menu.url"),
+    EDIT("menu.edit"),
+    WINDOW("menu.window"),
+    HELP("menu.help");
+
+    protected final String key;
+
+    DefaultMenuEnum(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public static DefaultMenuEnum[] getAll() {
+        return new DefaultMenuEnum[] {
+            FILE, URL, EDIT, WINDOW, HELP
+        };
+    }
+}
