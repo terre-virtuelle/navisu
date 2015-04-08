44d85e376f292c66b81a5197a2da79cf30cf396f
serge <serge@serge-XPS-8500>
2015-04-08 20:21:39 +0200

    intro : virtual sonar

--- navisu-widgets/src/main/java/bzh/terrevirtuelle/navisu/widgets/sonar/sonar3D/Points3D.java
+++ navisu-widgets/src/main/java/bzh/terrevirtuelle/navisu/widgets/sonar/sonar3D/Points3D.java
@@ -1,10 +1,14 @@
 package bzh.terrevirtuelle.navisu.widgets.sonar.sonar3D;
 
 import bzh.terrevirtuelle.navisu.domain.bathymetry.view.SHOM_BATHYMETRY_CLUT_JAVA_FX;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.geometry.Point3D;
+import javafx.scene.AmbientLight;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.PerspectiveCamera;
@@ -14,8 +18,17 @@ import javafx.scene.control.Tooltip;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.CullFace;
+import javafx.scene.shape.DrawMode;
+import javafx.scene.shape.MeshView;
 import javafx.scene.shape.Sphere;
+import javafx.scene.shape.TriangleMesh;
 import javafx.stage.Stage;
+
+import org.jdelaunay.delaunay.ConstrainedMesh;
+import org.jdelaunay.delaunay.error.DelaunayError;
+import org.jdelaunay.delaunay.geometries.DPoint;
+import org.jdelaunay.delaunay.geometries.DTriangle;
 
 /**
  *
@@ -43,6 +56,9 @@ public class Points3D extends Group {
     double maxLon;
     double minElevation;
     double maxElevation;
+    double centerX;
+    double centerY;
+    double centerZ;
 
     public Points3D(Stage primaryStage, List<Point3D> points,
             double minLat, double maxLat,
@@ -58,18 +74,20 @@ public class Points3D extends Group {
         Scene scene = new Scene(sceneRoot, sceneWidth, sceneHeight);
         scene.setFill(Color.BLACK);
         camera = new PerspectiveCamera(true);
-        camera.setNearClip(0.001);//0.1
+        camera.setNearClip(0.0001);//0.1
         camera.setFarClip(10000.0);//10000.0
-        camera.setTranslateZ(-70);//-1000
+        //   camera.setTranslateZ(-70);//-1000
         scene.setCamera(camera);
+        AmbientLight light = new AmbientLight();
+        light.setColor(Color.WHITE);
+        // Group pointsGroup = buildPoints(points);//100,100
+        Group pointsGroup = buildDelaunay(delaunay(points));
 
-        Group pointsGroup = buildPoints(points);//100,100
-        pointsGroup.setTranslateX(-6);//-400
-        pointsGroup.setTranslateY(-4);
+        pointsGroup.setTranslateX(-7+centerY);
+        pointsGroup.setTranslateY(-centerX/10);//-4
         pointsGroup.setRotationAxis(new Point3D(1.0, 0.0, 0.0));
-        pointsGroup.setRotate(120.0);
-        //   pointsGroup.setTranslateZ(-z);
-        //   System.out.println("x "+x+" "+y+" "+z);
+        pointsGroup.setRotate(100.0);
+
         sceneRoot.getChildren().addAll(pointsGroup);
         scene.setOnMouseClicked(event -> {
             Node picked = event.getPickResult().getIntersectedNode();
@@ -83,7 +101,7 @@ public class Points3D extends Group {
                 picked.setScaleZ(scalar);
             }
         });
-        //Start Tracking mouse movements only when a button is pressed
+
         scene.setOnMousePressed(event -> {
             scenex = event.getSceneX();
             sceney = event.getSceneY();
@@ -93,11 +111,6 @@ public class Points3D extends Group {
 
         scene.setOnKeyPressed(event -> {
             double change = cameraQuantity;
-            //Add shift modifier to simulate "Running Speed"
-            if (event.isShiftDown()) {
-                change = cameraModifier;
-            }
-            //What key did the user press?
             KeyCode keycode = event.getCode();
             //Step 2c: Add Zoom controls
             if (keycode == KeyCode.W) {
@@ -108,10 +121,12 @@ public class Points3D extends Group {
             }
             //Step 2d:  Add Strafe controls
             if (keycode == KeyCode.A) {
-                camera.setTranslateX(camera.getTranslateX() - change);
+                pointsGroup.setRotationAxis(new Point3D(0.0, 1.0, 0.0));
+                pointsGroup.setRotate(pointsGroup.getRotate() + rotateModifier);
             }
             if (keycode == KeyCode.D) {
-                camera.setTranslateX(camera.getTranslateX() + change);
+                pointsGroup.setRotationAxis(new Point3D(0.0, 1.0, 0.0));
+                pointsGroup.setRotate(pointsGroup.getRotate() - rotateModifier);
             }
             if (keycode == KeyCode.R) {
                 pointsGroup.setRotationAxis(new Point3D(1.0, 0.0, 0.0));
@@ -123,16 +138,12 @@ public class Points3D extends Group {
             }
 
         });
-        //Step 4b:  Add a Point light to show specular highlights
-        PointLight light = new PointLight(Color.WHITE);
+
         sceneRoot.getChildren().add(light);
-        light.setTranslateZ(-sceneWidth / 2);
-        light.setTranslateY(-sceneHeight / 2);
 
         primaryStage.setTitle("TriangleMeshes");
         primaryStage.setScene(scene);
         primaryStage.show();
-        //End Step 1
     }
 
     private Group buildPoints(
@@ -142,17 +153,82 @@ public class Points3D extends Group {
             Sphere sphere = new Sphere(0.1);
             sphere.setTranslateX((p.getY() - minLon) * 1000);
             sphere.setTranslateY((p.getX() - minLat) * 1000);
-            sphere.setTranslateZ(-p.getZ());
-            sphere.setMaterial(new PhongMaterial(SHOM_BATHYMETRY_CLUT_JAVA_FX.getColor(p.getZ())));
+            sphere.setTranslateZ(p.getZ());
+            sphere.setMaterial(new PhongMaterial(SHOM_BATHYMETRY_CLUT_JAVA_FX.getColor(p.getZ(), false)));
 
             Tooltip t = new Tooltip(Double.toString(p.getZ()));
             Tooltip.install(sphere, t);
-            System.out.println(sphere.getTranslateX() + "," + sphere.getTranslateY() + "," + sphere.getTranslateZ());
             return sphere;
         }).forEach((sphere) -> {
             group.getChildren().add(sphere);
         });
-         System.out.println("\n\n\n");
         return group;
+    }
+
+    private Group buildDelaunay(List<List<Point3D>> triangles) {
+        Group group = new Group();
+        TriangleMesh mesh = new TriangleMesh();
+        triangles.stream().forEach((subList) -> {
+            subList.stream().forEach((p) -> {
+                mesh.getPoints().addAll((float) (p.getY() - minLon) * 1000,
+                        (float) (p.getX() - minLat) * 1000,
+                        (float) p.getZ());
+            });
+        });
+
+        mesh.getTexCoords().addAll(0, 0);
+        int k = 0;
+        int size = triangles.size();
+        for (int i = 0; i < size; i++) {
+            for (int j = k; j < k + 3; j++) {
+                mesh.getFaces().addAll(i + j, 0);
+            }
+            k += 2;
+        }
+
+        MeshView meshView = new MeshView(mesh);
+        meshView.setDrawMode(DrawMode.LINE);
+        meshView.setCullFace(CullFace.NONE);
+        meshView.setMaterial(new PhongMaterial(SHOM_BATHYMETRY_CLUT_JAVA_FX.getColor(centerZ, false)));
+        group.getChildren().add(meshView);
+        return group;
+
+    }
+
+    private List<List<Point3D>> delaunay(List<Point3D> points) {
+        List<List<Point3D>> list = new ArrayList<>();
+        List<DPoint> dpList = new ArrayList<>();
+        ConstrainedMesh aMesh = new ConstrainedMesh();
+        aMesh.setPrecision(1.0e-3);
+        aMesh.setVerbose(true);
+        try {
+            for (Point3D p : points) {
+                dpList.add(new DPoint(p.getX(), p.getY(), p.getZ()));
+            }
+            aMesh.setPoints(dpList);
+            aMesh.processDelaunay();
+            List<DTriangle> triangles = aMesh.getTriangleList();
+            triangles.stream().filter((t) -> (t.checkTopology())).map((t) -> {
+                List<Point3D> l = new ArrayList<>();
+                for (int i = 0; i < 3; i++) {
+                    l.add(new Point3D(t.getPoint(i).getX(),
+                            t.getPoint(i).getY(),
+                            t.getPoint(i).getZ()));
+                }
+                return l;
+            }).forEach((l) -> {
+                list.add(l);
+            });
+            centerX = aMesh.getBoundingBox().getMiddle().getX();
+            centerY = aMesh.getBoundingBox().getMiddle().getY();
+            centerZ = aMesh.getBoundingBox().getMiddle().getZ();
+
+            System.out.println(centerX + " " + centerY + "  " + centerZ);
+
+        } catch (DelaunayError ex) {
+            Logger.getLogger(Points3D.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return list;
     }
 }
