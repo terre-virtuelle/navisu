03ec4b3e891126075b42ea487080460a16b68862
serge <serge@serge-XPS-8500>
2015-04-12 21:13:19 +0200

    create : Target

---
+++ navisu-agents/src/main/java/bzh/terrevirtuelle/navisu/agents/target/TargetServices.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bzh.terrevirtuelle.navisu.agents.target;
+
+import org.capcaval.c3.component.ComponentService;
+
+/**
+ * NaVisu
+ *
+ * @date 10 avr. 2015
+ * @author Serge Morvan
+ */
+public interface TargetServices
+        extends ComponentService {
+
+}
