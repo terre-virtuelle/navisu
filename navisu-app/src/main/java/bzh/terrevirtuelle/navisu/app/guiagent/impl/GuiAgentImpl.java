ffea7484227e45a37f207e80fd2fcd721f995646
Mhyrdin <contact@mhyrdin.fr>
2015-04-15 15:32:28 +0200

    correction CSS pour usage fontes personnalisées

--- navisu-app/src/main/java/bzh/terrevirtuelle/navisu/app/guiagent/impl/GuiAgentImpl.java
+++ navisu-app/src/main/java/bzh/terrevirtuelle/navisu/app/guiagent/impl/GuiAgentImpl.java
@@ -159,8 +159,9 @@ public class GuiAgentImpl
                 getJobsManager().newJob("", (progressHandle) -> {
                     if (first == true) {
                         javafx.scene.media.Media media;
-                        //String url = System.getProperty("user.dir") + "/data/sounds/alarm10.wav";
-                        String url = "E:/Documents/Navisu/2015/navisu-wip/navisu-launcher/data/sounds/alarm10.wav";
+                        String url = System.getProperty("user.dir") + "/data/sounds/alarm10.wav";
+                        System.out.println("url-dom = " + url);
+                        //String url = "E:/Documents/Navisu/2015/navisu-wip/navisu-launcher/data/sounds/alarm10.wav";
                         media = new Media("file:///" + url);
                         mediaPlayer = new MediaPlayer(media);
                         mediaPlayer.setAutoPlay(true);
