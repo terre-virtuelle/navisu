54a8cb80eba22fc629fe0eaa4efa8af584168b4f
Mhyrdin <contact@mhyrdin.fr>
2015-04-20 12:35:08 +0200

    Merge origin/master
    Conflicts:
    	navisu-app/src/main/java/bzh/terrevirtuelle/navisu/app/guiagent/dock/impl/DockManagerImpl.java
    	navisu-launcher/src/main/java/bzh/terrevirtuelle/navisu/main/AppMain.java

---
+++ navisu-instruments/src/main/java/bzh/terrevirtuelle/navisu/instruments/gps/logger/GpsLoggerServices.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bzh.terrevirtuelle.navisu.instruments.gps.logger;
+
+import bzh.terrevirtuelle.navisu.app.drivers.instrumentdriver.InstrumentDriver;
+import org.capcaval.c3.component.ComponentService;
+
+/**
+ * @date 3 mars 2015
+ * @author Serge Morvan
+ */
+public interface GpsLoggerServices extends ComponentService {
+
+    void on();
+
+    default void off() {
+    }
+
+    boolean isOn();
+
+    boolean canOpen(String category);
+
+    InstrumentDriver getDriver();
+}
