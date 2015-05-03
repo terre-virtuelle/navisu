67946011116bed6dba713bd71b5abbffb33658a4
serge <serge@serge-XPS-8500>
2015-05-03 19:55:24 +0200

    create   : new module navisu-speech

---
+++ navisu-speech/src/main/java/bzh/terrevirtuelle/navisu/speech/Speaker.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bzh.terrevirtuelle.navisu.speech;
+
+import org.capcaval.c3.component.Component;
+
+/**
+ * NaVisu
+ *
+ * @date 3 mai 2015
+ * @author Serge Morvan
+ */
+public interface Speaker
+        extends Component {
+
+}
