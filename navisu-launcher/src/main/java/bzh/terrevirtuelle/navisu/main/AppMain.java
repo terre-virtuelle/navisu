5190b8a92a0ff3640504f4aec40c3d623e4ca924
serge <serge@serge-XPS-8500>
2015-05-28 20:48:59 +0200

    create : TrackPanel

--- navisu-launcher/src/main/java/bzh/terrevirtuelle/navisu/main/AppMain.java
+++ navisu-launcher/src/main/java/bzh/terrevirtuelle/navisu/main/AppMain.java
@@ -336,8 +336,8 @@ public class AppMain extends Application {
         //dataServerServices.openFile("data/ais/ais.txt");  //AIS
         
         // Test cibles AIS en direct
-        dataServerServices.openGpsd("sinagot.net", 2947);
-        //dataServerServices.openGpsd("fridu.net", 2947);
+      dataServerServices.openGpsd("sinagot.net", 2947);
+      //  dataServerServices.openGpsd("fridu.net", 2947);
         
        // testDBServices.connect("data/databases/TestJDBC", "navisu", "!!navisu??");
        // testDBServices.runJDBC();//OK
