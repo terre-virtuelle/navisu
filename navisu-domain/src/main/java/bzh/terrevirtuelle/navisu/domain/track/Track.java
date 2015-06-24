990b2ef65750ce2cc729642615af5bdafc2e175a
serge <serge@serge-XPS-8500>
2015-06-24 18:59:47 +0200

    modif  : Track

---
+++ navisu-domain/src/main/java/bzh/terrevirtuelle/navisu/domain/track/Track.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bzh.terrevirtuelle.navisu.domain.track;
+
+import bzh.terrevirtuelle.navisu.domain.util.Pair;
+import java.util.List;
+
+/**
+ * NaVisu
+ *
+ * @date 23 juin 2015
+ * @author Serge Morvan
+ */
+public class Track {
+
+    List<Pair<Double, Double>> data;
+}
---
+++ navisu-domain/src/main/java/bzh/terrevirtuelle/navisu/domain/util/Pair.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bzh.terrevirtuelle.navisu.domain.util;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Serge Morvan
+ * @date 4 oct. 2014
+ * NaVisu project
+ */
+public class Pair<T,K>{
+
+    private T x;
+
+    private K y;
+
+    public Pair() {
+    }
+
+    public Pair(T x, K y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Get the value of y
+     *
+     * @return the value of y
+     */
+    public K getY() {
+        return y;
+    }
+
+    /**
+     * Set the value of y
+     *
+     * @param y new value of y
+     */
+    public void setY(K y) {
+        this.y = y;
+    }
+
+    /**
+     * Get the value of x
+     *
+     * @return the value of x
+     */
+    public T getX() {
+        return x;
+    }
+
+    /**
+     * Set the value of x
+     *
+     * @param x new value of x
+     */
+    public void setX(T x) {
+        this.x = x;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pair<?, ?> other = (Pair<?, ?>) obj;
+        if (!Objects.equals(this.x, other.x)) {
+            return false;
+        }
+        if (!Objects.equals(this.y, other.y)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" + "x=" + x + ", y=" + y + '}';
+    }
+
+    
+
+   
+}
