e87648230102a228f745c3995e89bf1e300490b1
serge <serge@serge-XPS-8500>
2015-06-25 21:06:49 +0200

    test  : smooth Bezier OK

--- navisu-geometry/src/main/java/bzh/terrevirtuelle/navisu/geometry/utils/Utils.java
+++ navisu-geometry/src/main/java/bzh/terrevirtuelle/navisu/geometry/utils/Utils.java
@@ -11,15 +11,12 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 import java.util.stream.LongStream;
-import java.util.stream.Stream;
 
 /**
  * NaVisu
