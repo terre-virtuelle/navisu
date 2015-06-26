46bd3024a09e7f9868cc1fc78a4f6e2f53e74845
VivienAtol <vivien.bontoux@telecom-bretagne.eu>
2015-06-26 14:22:23 +0200

    Sauvegarde de routine.

--- navisu-launcher/src/main/java/bzh/terrevirtuelle/navisu/main/AppMain.java
+++ navisu-launcher/src/main/java/bzh/terrevirtuelle/navisu/main/AppMain.java
@@ -328,7 +328,7 @@ public class AppMain extends Application {
         //gpsLoggerServices.on("data/nmea/test2.txt");
         //gpsPlotterServices.on();
 
-        /*List<Pair<Double, Double>> data = bezier2DServices.readCsv("data/saved/", "testBez.csv");
+        /*List<Pair<Double, Double>> data = bezier2DServices.readCsv("data/saved/", "savedPath.csv");
         bezier2DServices.toKML("path.kml", data);
         
         List<Pair<Double, Double>> bez = bezier2DServices.leastSquareCompute(data, 0.01, 8);
