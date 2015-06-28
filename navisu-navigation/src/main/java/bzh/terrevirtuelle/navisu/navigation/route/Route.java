d0b0c7b43b2a047d5a1dcb2d6a6dbd1a6ab81df2
serge <serge@serge-XPS-8500>
2015-06-28 11:45:11 +0200

    create  : navisu-navigation module

---
+++ navisu-navigation/src/main/java/bzh/terrevirtuelle/navisu/navigation/route/Route.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bzh.terrevirtuelle.navisu.navigation.route;
+
+import org.capcaval.c3.component.Component;
+
+/**
+ * NaVisu
+ *
+ * @date 27 juin 2015
+ * @author Serge Morvan
+ */
+public interface Route
+        extends Component {
+
+}
---
+++ navisu-navigation/src/main/java/bzh/terrevirtuelle/navisu/navigation/route/RouteServices.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bzh.terrevirtuelle.navisu.navigation.route;
+
+import bzh.terrevirtuelle.navisu.util.Pair;
+import java.util.List;
+import org.capcaval.c3.component.ComponentService;
+
+/**
+ * NaVisu
+ *
+ * @date 27 juin 2015
+ * @author Serge Morvan
+ */
+public interface RouteServices
+        extends ComponentService {
+
+   }
