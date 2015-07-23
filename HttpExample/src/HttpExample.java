45966b062c6194ee2d7c1061470f00b06eb6d943
serge <serge@serge-XPS-8500>
2015-07-23 21:20:49 +0200

    create  : Ontology module

---
+++ HttpExample/src/HttpExample.java
@@ -0,0 +1,111 @@
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import java.util.List;
+import java.util.Map;
+
+public class HttpExample {
+
+    private final String USER_AGENT = "Mozilla/5.0";
+
+    public static void main(String[] args) throws Exception {
+
+        HttpExample http = new HttpExample();
+
+        System.out.println("Testing 1 - Send Http GET request");
+        http.sendGet();
+
+        System.out.println("\nTesting 2 - Send Http POST request");
+        http.sendPost();
+
+    }
+
+    // HTTP GET request
+    private void sendGet() throws Exception {
+
+        String url = "http://commons.wikimedia.org/wiki/Special:FilePath/Phare_de_Kermorvan_vu_de_la_presqu'île.jpg";
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        //add request header
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+        Map<String, List<String>> map = con.getHeaderFields();
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            System.out.println("Key : " + entry.getKey()
+                    + " ,Value : " + entry.getValue());
+        }
+    }
+
+    // HTTP POST request
+    private void sendPost() throws Exception {
+
+        String url = "http://commons.wikimedia.org/wiki/Special:FilePath/Phare_de_Kermorvan_vu_de_la_presqu'île.jpg";
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        //add reuqest header
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+        String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
+        Map<String, List<String>> map = con.getHeaderFields();
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            System.out.println("Key : " + entry.getKey()
+                    + " ,Value : " + entry.getValue());
+        }
+
+    }
+
+}
