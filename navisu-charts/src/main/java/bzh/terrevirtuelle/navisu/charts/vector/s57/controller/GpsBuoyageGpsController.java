6d66fefdbfdb85ae65f48aadd2d27523c6c1f6ec
serge <serge@serge-XPS-8500>
2015-09-12 18:09:56 +0200

    create  : ExtendedBuoyage for recept Gps evts

---
+++ navisu-charts/src/main/java/bzh/terrevirtuelle/navisu/charts/vector/s57/controller/GpsBuoyageGpsController.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bzh.terrevirtuelle.navisu.charts.vector.s57.controller;
+
+import bzh.terrevirtuelle.navisu.instruments.common.controller.GpsEventsController;
+
+/**
+ * NaVisu
+ *
+ * @date 12 sept. 2015
+ * @author Serge Morvan
+ */
+public class GpsBuoyageGpsController 
+               extends GpsEventsController{
+
+}
---
+++ navisu-charts/src/main/java/bzh/terrevirtuelle/navisu/charts/vector/s57/model/ExtendedBuoyage.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bzh.terrevirtuelle.navisu.charts.vector.s57.model;
+
+import bzh.terrevirtuelle.navisu.domain.charts.vector.s57.model.geo.Buoyage;
+
+/**
+ * NaVisu
+ *
+ * @date 12 sept. 2015
+ * @author Serge Morvan
+ */
+public class ExtendedBuoyage extends Buoyage{
+
+}
