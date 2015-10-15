778e700224e6e81ff920c6cf278672f6b8a87487
Arnaud Grancher <agrancher@gmail.com>
2015-10-15 22:26:01 +0200

    [navisu-ontology] création du controller DataAccessController.

---
+++ navisu-ontology/src/main/java/bzh/terrevirtuelle/navisu/ontology/data/impl/controller/DataAccessController.java
@@ -0,0 +1,11 @@
+package bzh.terrevirtuelle.navisu.ontology.data.impl.controller;
+
+/**
+ * NaVisu
+ *
+ * @date 15 oct. 2015
+ * @author Arnaud Grancher
+ */
+public class DataAccessController {
+
+}
