56660c870818b38fdf0f79d008407032721763fd
serge <serge@serge-XPS-8500>
2015-10-28 20:40:22 +0100

    create : ExifType type

---
+++ navisu-domain/src/main/java/bzh/terrevirtuelle/navisu/domain/photos/exif/ExifType.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bzh.terrevirtuelle.navisu.domain.photos.exif;
+
+/**
+ * NaVisu
+ *
+ * @date 28 oct. 2015
+ * @author Serge Morvan
+ */
+public class ExifType {
+
+}
