eab05d41bb22df780e73ba0ba4170d4fbcf99dc3
Serge <sergemorvan29@gmail.com>
2015-12-24 13:50:25 +0100

    refactor : Gps Track

---
+++ navisu-navigation/src/main/java/bzh/terrevirtuelle/navisu/navigation/controller/behavior/S57ChartBehavior.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bzh.terrevirtuelle.navisu.navigation.controller.behavior;
+
+import bzh.terrevirtuelle.navisu.charts.vector.s57.charts.impl.controller.navigation.S57Behavior;
+
+/**
+ *
+ * @author serge
+ * @date 24 déc. 2015
+ */
+public class S57ChartBehavior
+        extends S57Behavior {
+
+    @Override
+    public void doIt(double distance, double azimuth) {
+    }
+
+}
