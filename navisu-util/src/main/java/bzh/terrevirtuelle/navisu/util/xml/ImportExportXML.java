545e0e6e5e177d8362cc27a28d0d1deaba0f1721
serge <serge@serge-XPS-8500>
2016-01-15 11:59:02 +0100

    add :  JAXB importExport <T> whith StringWriter and StringReader

--- navisu-util/src/main/java/bzh/terrevirtuelle/navisu/util/xml/ImportExportXML.java
+++ navisu-util/src/main/java/bzh/terrevirtuelle/navisu/util/xml/ImportExportXML.java
@@ -9,6 +9,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
@@ -47,6 +49,15 @@ public class ImportExportXML<T> {
         return data;
     }
 
+    public static <T> T exports(T data, StringWriter xmlString) throws JAXBException {
+        JAXBContext jAXBContext;
+        Marshaller marshaller;
+        jAXBContext = JAXBContext.newInstance(data.getClass());
+        marshaller = jAXBContext.createMarshaller();
+        marshaller.marshal(jAXBContext, xmlString);
+        return data;
+    }
+
     public static <T> T imports(T data, File file) throws FileNotFoundException, JAXBException {
         if (file != null) {
             FileInputStream inputFile = new FileInputStream(new File(file.getPath()));
@@ -67,6 +78,15 @@ public class ImportExportXML<T> {
         jAXBContext = JAXBContext.newInstance(data.getClass());
         unmarshaller = jAXBContext.createUnmarshaller();
         data = (T) unmarshaller.unmarshal(inputFile);
+        return data;
+    }
+
+    public static <T> T imports(T data, StringReader xmlString) throws JAXBException {
+        Unmarshaller unmarshaller;
+        JAXBContext jAXBContext;
+        jAXBContext = JAXBContext.newInstance(data.getClass());
+        unmarshaller = jAXBContext.createUnmarshaller();
+        data = (T) unmarshaller.unmarshal(xmlString);
         return data;
     }
 }
