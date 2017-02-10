e5da52d545da40c5c9e2fc01a8754cb7b89b2469
serge <serge@serge-XPS-8500>
2017-02-10 17:30:33 +0100

    Intro TextEditorComponent

---
+++ navisu-texteditor/src/main/java/bzh/terrevirtuelle/navisu/texteditor/TextEditorComponentServices.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bzh.terrevirtuelle.navisu.texteditor;
+
+import java.awt.Image;
+import java.util.List;
+import javafx.scene.image.ImageView;
+
+import org.capcaval.c3.component.ComponentService;
+
+/**
+ * @author serge Date: 9/2/2017
+ */
+public interface TextEditorComponentServices
+        extends ComponentService {
+
+    List<Image> convertPDFFileToImages(String filename, int width, int height, String extension);
+
+    void pageFlipper(List<Image> images);
+}
