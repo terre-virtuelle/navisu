d0e56380fad000c6e9d398be466e607d214bbaab
serge <serge@serge-XPS-8500>
2017-02-19 15:31:07 +0100

    Use config.properties

--- navisu-launcher/src/main/java/bzh/terrevirtuelle/navisu/main/Configuration.java
+++ navisu-launcher/src/main/java/bzh/terrevirtuelle/navisu/main/Configuration.java
@@ -7,7 +7,6 @@ package bzh.terrevirtuelle.navisu.main;
 
 import bzh.terrevirtuelle.navisu.weather.impl.darksky.controller.DarkSkyComponentController;
 import bzh.terrevirtuelle.navisu.weather.impl.darksky.controller.DarkSkyController;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.lang.reflect.Field;
@@ -22,7 +21,6 @@ import java.util.List;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.stage.FileChooser;
 
 /**
  *
@@ -63,26 +61,46 @@ public class Configuration {
             }
         }
 
-        String navisuWeatherCache = navisuHome + "/caches/weather.properties";
-        if (!Files.exists(Paths.get(navisuWeatherCache), LinkOption.NOFOLLOW_LINKS)) {
+        String navisuCache = navisuHome + "/caches/caches.properties";
+        if (!Files.exists(Paths.get(navisuCache), LinkOption.NOFOLLOW_LINKS)) {
             try {
-                Files.createFile(Paths.get(navisuWeatherCache));
+                Files.createFile(Paths.get(navisuCache));
             } catch (IOException ex) {
                 Logger.getLogger(Configuration.class.getName()).log(Level.SEVERE, null, ex);
             }
-            writeDefaultWeatherCache(navisuWeatherCache);
+            writeDefaultCache(navisuCache);
         } else {
             Properties properties = new Properties();
             try {
-                properties.load(new FileInputStream(navisuWeatherCache));
+                properties.load(new FileInputStream(navisuCache));
                 String town = properties.getProperty("town");
                 String language = properties.getProperty("language");
                 String unit = properties.getProperty("unit");
                 String country = properties.getProperty("country");
                 String countryCode = properties.getProperty("countryCode");
 
-                if (town == null || language == null || unit == null || country == null || countryCode == null) {
-                    writeDefaultWeatherCache(navisuWeatherCache);
+                String kml = properties.getProperty("KML");
+                String meteo = properties.getProperty("Meteo");
+                String s57 = properties.getProperty("S57");
+                String gpx = properties.getProperty("Gpx");
+                String meteoDump = properties.getProperty("MeteoDump");
+                String bsbKap = properties.getProperty("BSB/KAP");
+                String wind = properties.getProperty("Wind");
+                String nmea = properties.getProperty("NMEA");
+                String waves = properties.getProperty("Waves");
+                String magnetic = properties.getProperty("Magnetic");
+                String bathy = properties.getProperty("Bathy");
+                String netCdfInfo = properties.getProperty("netCdfInfo");
+                String geoTiff = properties.getProperty("GeoTiff");
+                String currents = properties.getProperty("Currents");
+                String dataDir = properties.getProperty("dataDir");
+
+                if (town == null || language == null || unit == null || country == null || countryCode == null
+                        || kml == null || meteo == null || s57 == null || gpx == null || meteoDump == null
+                        || bsbKap == null || wind == null || nmea == null
+                        || waves == null || magnetic == null || bathy == null || netCdfInfo == null || geoTiff == null
+                        || currents == null || dataDir == null) {
+                    writeDefaultCache(navisuCache);
                 }
             } catch (IOException ex) {
                 Logger.getLogger(DarkSkyComponentController.class.getName()).log(Level.SEVERE, ex.toString(), ex);
@@ -108,7 +126,7 @@ public class Configuration {
 
                 if (s57ChartsDir == null || darkSkyApiKey == null
                         || allCountriesPath == null || luceneAllCountriesIndexPath == null) {
-                    writeDefaultConfigProperties(navisuWeatherCache);
+                    writeDefaultConfigProperties(navisuCache);
                 }
             } catch (IOException ex) {
                 Logger.getLogger(DarkSkyComponentController.class.getName()).log(Level.SEVERE, ex.toString(), ex);
@@ -117,10 +135,14 @@ public class Configuration {
 
     }
 
-    private static void writeDefaultWeatherCache(String navisuWeatherCache) {
+    private static void writeDefaultCache(String navisuWeatherCache) {
         try {
             List<String> keys = new ArrayList<>(Arrays.asList("town=",
-                    "language=", "unit=", "country=", "countryCode="));
+                    "language=", "unit=", "country=", "countryCode=",
+                    "KML=", "Meteo=", "S57", "Gpx=", "MeteoDump=", "BSB/KAP=",
+                    "Wind=", "NMEA=", "Waves=", "Magnetic=", "Bathy=", "NetCdfInfo=",
+                    "GeoTiff=", "Currents=", "dataDir="
+            ));
             Files.write(Paths.get(navisuWeatherCache), keys, StandardOpenOption.WRITE);
         } catch (IOException ex) {
             Logger.getLogger(DarkSkyController.class.getName()).log(Level.SEVERE, ex.toString(), ex);
@@ -131,16 +153,16 @@ public class Configuration {
         System.out.println("writeDefaultConfigProperties");
         try {
             List<String> keys = new ArrayList<>(Arrays.asList(
-                    "s57ChartsDir=","darkSkyApiKey=", "allCountriesPath=", "luceneAllCountriesIndexPath=",
-                    "name","mmsi","country","length","width","draught","shipType","navigationalStatus",
-                    "callSign","latitude","longitude","cog","sog","daeModelPath"
-                    ));
+                    "s57ChartsDir=", "darkSkyApiKey=", "allCountriesPath=", "luceneAllCountriesIndexPath=",
+                    "name", "mmsi", "country", "length", "width", "draught", "shipType", "navigationalStatus",
+                    "callSign", "latitude", "longitude", "cog", "sog", "daeModelPath"
+            ));
             Files.write(Paths.get(configProperties), keys, StandardOpenOption.WRITE);
         } catch (IOException ex) {
             Logger.getLogger(DarkSkyController.class.getName()).log(Level.SEVERE, ex.toString(), ex);
         }
     }
-    
+
     /**
      * Adds the specified path to the java library path
      *
@@ -162,6 +184,5 @@ public class Configuration {
         newPaths[newPaths.length - 1] = pathToAdd;
         usrPathsField.set(null, newPaths);
     }
-    
-    
+
 }
