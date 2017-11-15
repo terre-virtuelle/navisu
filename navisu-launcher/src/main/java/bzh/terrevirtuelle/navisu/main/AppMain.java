7f2d0a047cb1432cf5cd7306cc07620ac9db87da
serge <serge@serge-XPS-8500>
2017-11-15 20:51:02 +0100

    Transfert logs to $HOME/.navisu/logs

--- navisu-launcher/src/main/java/bzh/terrevirtuelle/navisu/main/AppMain.java
+++ navisu-launcher/src/main/java/bzh/terrevirtuelle/navisu/main/AppMain.java
@@ -494,8 +494,8 @@ public class AppMain extends Application {
         // Neo4J serveur externe
         // Connection con = testDBServices.connect("localhost", "jdbc:neo4j://", "7474", "org.neo4j.jdbc.Driver", "root", "lithops");
         // System.out.println("con : " + con);
-        //bathymetryDBServices.connect("BathyShomDB", "localhost", "jdbc:postgresql://",
-        //        "5432", "org.postgresql.Driver", "admin", "admin");
+        bathymetryDBServices.connect("BathyShomDB", "localhost", "jdbc:postgresql://",
+                "5432", "org.postgresql.Driver", "admin", "admin");
         //  bathymetryDBServices.create("C:\\Users\\Dom\\Documents\\Navisu\\shom-data\\bathy\\MNT100M_ATL\\splited\\bathy04.glz");
         //bathymetryDBServices.createIndex();
         /* Test speech */
