77ebcc47b48a8ffcccfa5d2106461e96833aaa58
serge <serge@serge-XPS-8500>
2018-02-18 12:32:07 +0100

    Update S57DBComponentController

--- navisu-charts/src/main/java/bzh/terrevirtuelle/navisu/charts/vector/s57/databases/impl/S57DBComponentImpl.java
+++ navisu-charts/src/main/java/bzh/terrevirtuelle/navisu/charts/vector/s57/databases/impl/S57DBComponentImpl.java
@@ -1,14 +1,14 @@
 package bzh.terrevirtuelle.navisu.charts.vector.s57.databases.impl;
 
 import bzh.terrevirtuelle.navisu.api.progress.ProgressHandle;
-import bzh.terrevirtuelle.navisu.app.drivers.driver.Driver;
 import bzh.terrevirtuelle.navisu.app.drivers.instrumentdriver.InstrumentDriver;
+import bzh.terrevirtuelle.navisu.app.drivers.instrumentdriver.InstrumentDriverManagerServices;
 import bzh.terrevirtuelle.navisu.app.guiagent.GuiAgentServices;
 
 import bzh.terrevirtuelle.navisu.app.guiagent.geoview.GeoViewServices;
 import bzh.terrevirtuelle.navisu.app.guiagent.layers.LayersManagerServices;
 import bzh.terrevirtuelle.navisu.app.guiagent.layertree.LayerTreeServices;
-import bzh.terrevirtuelle.navisu.charts.vector.s57.catalog.global.S57GlobalCatalogServices;
+import bzh.terrevirtuelle.navisu.charts.vector.s57.charts.S57ChartComponentServices;
 import bzh.terrevirtuelle.navisu.core.view.geoview.worldwind.impl.GeoWorldWindViewImpl;
 import gov.nasa.worldwind.WorldWindow;
 import gov.nasa.worldwind.layers.Layer;
@@ -23,6 +23,7 @@ import bzh.terrevirtuelle.navisu.instruments.transponder.impl.events.Transponder
 import bzh.terrevirtuelle.navisu.charts.vector.s57.databases.S57DBComponent;
 import bzh.terrevirtuelle.navisu.charts.vector.s57.databases.S57DBComponentServices;
 import bzh.terrevirtuelle.navisu.charts.vector.s57.databases.impl.controller.S57DBComponentController;
+import bzh.terrevirtuelle.navisu.database.relational.DatabaseServices;
 import bzh.terrevirtuelle.navisu.domain.charts.vector.s57.model.geo.Buoyage;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCombination;
@@ -44,8 +45,11 @@ public class S57DBComponentImpl
     @UsedService
     LayersManagerServices layersManagerServices;
     @UsedService
-    S57GlobalCatalogServices s57GlobalCatalogServices;
-
+    S57ChartComponentServices s57ChartComponentServices;
+    @UsedService
+    DatabaseServices databaseServices;
+    @UsedService
+    InstrumentDriverManagerServices instrumentDriverManagerServices;
     ComponentManager cm;
     ComponentEventSubscribe<TransponderActivateEvent> transponderActivateEvent;
 
@@ -73,10 +77,10 @@ public class S57DBComponentImpl
         if (cmd != null) {
             componentKeyName = cmd[0];
             if (cmd[0].equals(COMPONENT_KEY_NAME_0)) {
-            //    controller = new S57DBComponentController(this, componentKeyName, KeyCode.T, KeyCombination.CONTROL_DOWN,
-            //            guiAgentServices, s57ChartComponentServices,
-            //            databaseServices, instrumentDriverManagerServices);
-            //    controller.setVisible(true);
+                controller = new S57DBComponentController(this, componentKeyName, KeyCode.T, KeyCombination.CONTROL_DOWN,
+                        guiAgentServices, s57ChartComponentServices,
+                        databaseServices, instrumentDriverManagerServices);
+                controller.setVisible(true);
             }
         }
     }
@@ -105,7 +109,6 @@ public class S57DBComponentImpl
 
     }
 
-   
     @Override
     public InstrumentDriver getDriver() {
         return this;
--- navisu-charts/src/main/java/bzh/terrevirtuelle/navisu/charts/vector/s57/databases/impl/controller/S57DBComponentController.java
+++ navisu-charts/src/main/java/bzh/terrevirtuelle/navisu/charts/vector/s57/databases/impl/controller/S57DBComponentController.java
@@ -9,9 +9,11 @@ import bzh.terrevirtuelle.navisu.app.drivers.instrumentdriver.InstrumentDriverMa
 import bzh.terrevirtuelle.navisu.app.guiagent.GuiAgentServices;
 import bzh.terrevirtuelle.navisu.app.guiagent.layers.LayersManagerServices;
 import bzh.terrevirtuelle.navisu.app.guiagent.layertree.LayerTreeServices;
+import bzh.terrevirtuelle.navisu.charts.vector.s57.charts.S57ChartComponentServices;
 import bzh.terrevirtuelle.navisu.charts.vector.s57.charts.impl.controller.navigation.S57Controller;
 import bzh.terrevirtuelle.navisu.charts.vector.s57.databases.impl.S57DBComponentImpl;
 import bzh.terrevirtuelle.navisu.core.view.geoview.worldwind.impl.GeoWorldWindViewImpl;
+import bzh.terrevirtuelle.navisu.database.relational.DatabaseServices;
 import gov.nasa.worldwind.WorldWindow;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -25,9 +27,35 @@ import java.util.logging.Logger;
 import bzh.terrevirtuelle.navisu.widgets.impl.Widget2DController;
 import gov.nasa.worldwind.layers.RenderableLayer;
 import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Properties;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 
 /**
  * @author Serge Morvan
@@ -38,19 +66,51 @@ public class S57DBComponentController
         implements Initializable {
 
     protected S57DBComponentImpl component;
-    protected LayersManagerServices layersManagerServices;
+    private String componentKeyName;
+    protected static final Logger LOGGER = Logger.getLogger(S57DBComponentController.class.getName());
     protected GuiAgentServices guiAgentServices;
-    protected InstrumentDriverManagerServices instrumentDriverManagerServices;
     protected LayerTreeServices layerTreeServices;
+    protected LayersManagerServices layersManagerServices;
+    protected DatabaseServices databaseServices;
+    protected InstrumentDriverManagerServices instrumentDriverManagerServices;
+
+    private final String FXML = "s57DBController.fxml";
+
+    protected String CONFIG_FILE_NAME = System.getProperty("user.home") + "/.navisu/config/config.properties";
     protected static final String ALARM_SOUND = "/data/sounds/pling.wav";
     protected static final String DATA_PATH = System.getProperty("user.dir").replace("\\", "/");
-    // Pour récupérer le .css
-    protected final String FXML = "configurationStlController.fxml";
-    //protected String VIEW_GROUP_STYLE = "configuration.css";
-    protected String VIEW_GROUP_STYLE = "common.css";
-    protected static final String CSS_STYLE_PATH = Paths.get(System.getProperty("user.dir") + "/css/").toUri().toString();
-   
-    protected static S57DBComponentController INSTANCE = null;
+    private final String USER = "admin";
+    private final String PASSWD = "admin";
+    protected Properties properties;
+
+    private static final String CSS_STYLE_PATH = Paths.get(System.getProperty("user.dir") + "/css/").toUri().toString();
+    protected String viewgroupstyle = "configuration.css";
+
+    /* Common controls */
+    @FXML
+    public Group view;
+    @FXML
+    public Pane viewPane;
+    @FXML
+    public TabPane toolsTabPane;
+    @FXML
+    public Pane quit;
+    @FXML
+    public Button requestButton;
+    @FXML
+    public Button helpButton;
+    @FXML
+    public ChoiceBox<String> databasesCB;
+    @FXML
+    public ChoiceBox<String> objectsCB;
+    @FXML
+    public Button latLonButton;
+    @FXML
+    public Button interactiveButton;
+    @FXML
+    public TextField hostnameTF;
+    @FXML
+    public TextField encPortDBTF;
 
     protected Map<String, String> acronyms;
     protected WorldWindow wwd = GeoWorldWindViewImpl.getWW();
@@ -60,24 +120,78 @@ public class S57DBComponentController
     protected final Set<S57Controller> s57Controllers = new HashSet<>();
     protected boolean first = true;
     protected RenderableLayer layer;
-    
+    protected double lat0;
+    protected double lon0;
+    protected double lat1;
+    protected double lon1;
 
-    public S57DBComponentController(S57DBComponentImpl component,
+    private ObservableList<String> dbCbData = FXCollections.observableArrayList("s57NP1DB", "s57NP2DB", "s57NP3DB", "s57NP4DB", "s57NP5DB", "s57NP6DB");
+    private ObservableList<String> objectsCbData = FXCollections.observableArrayList("ALL:All objects",
+            "BUOYAGE:All buoyage",
+            "ACHARE:AnchorageArea",
+            "BCNCAR:BeaconCardinal",
+            "BCNISD:BeaconIsolatedDanger",
+            "BCNLAT:BeaconLateral",
+            "BCNSAW:BeaconSafeWater",
+            "BCNSPP:BeaconSpecialPurposeGeneral",
+            "BOYCAR:BuoyCardinal",
+            "BOYINB:BuoyInstallation",
+            "BOYISD:BuoyIsolatedDanger",
+            "BOYLAT:BuoyLateral",
+            "BOYSAW:BuoySafeWater",
+            "BOYSPP:BuoySpecial",
+            "DAYMAR:Daymark",
+            "DEPARE:DepthArea",
+            "DEPCNT:DepthContour",
+            "DOCARE:DockArea",
+            "DRGARE:DredgedArea",
+            "FAIRWY:Fairway",
+            "LAKARE:Lake",
+            "LIGHTS:Light",
+            "LNDMRK:Landmark",
+            "MIPARE:MilitaryPracticeArea",
+            "MORFAC:MooringWarpingFacility",
+            "NAVLNE:NavigationLine",
+            "OBSTRN:NavigationalSystemOfMarks",
+            "PONTON:Pontoon",
+            "RESARE:RestrictedArea",
+            "SEAARE:SeaAreaNamedWaterArea",
+            "SLCONS:ShorelineConstruction",
+            "SOUNDG:Sounding",
+            "TOPMAR:Topmark",
+            "TSSBND:TrafficSeparationSchemeBoundary",
+            "UNSARE:UnsurveyedArea",
+            "UWTROC:UnderwaterAwashRock",
+            "WRECKS:Wreck");
+
+    public S57DBComponentController(S57DBComponentImpl component, String componentKeyName,
+            KeyCode keyCode, KeyCombination.Modifier keyCombination,
             GuiAgentServices guiAgentServices,
-            LayerTreeServices layerTreeServices,
-            LayersManagerServices layersManagerServices,
-            InstrumentDriverManagerServices instrumentDriverManagerServices,
-            String GROUP,
-            String NAME,
-            WorldWindow wwd) {
+            S57ChartComponentServices s57ChartComponentServices,
+            DatabaseServices databaseServices,
+            InstrumentDriverManagerServices instrumentDriverManagerServices) {
+        super(keyCode, keyCombination);
+        this.componentKeyName = componentKeyName;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(FXML));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+        try {
+            fxmlLoader.load();
+        } catch (IOException ex) {
+            LOGGER.log(Level.SEVERE, ex.toString(), ex);
+        }
+        String uri = CSS_STYLE_PATH + viewgroupstyle;
+        view.getStylesheets().add(uri);
         this.component = component;
         this.guiAgentServices = guiAgentServices;
-        this.layersManagerServices = layersManagerServices;
-        this.layerTreeServices = layerTreeServices;
+        // this.s57ChartComponentServices = s57ChartComponentServices;
+        this.databaseServices = databaseServices;
         this.instrumentDriverManagerServices = instrumentDriverManagerServices;
-        
-        this.wwd = wwd;
-        layer = layersManagerServices.getLayer(GROUP, NAME);
+
+        guiAgentServices.getScene().addEventFilter(KeyEvent.KEY_RELEASED, this);
+        guiAgentServices.getRoot().getChildren().add(this);
+        //    layer = layersManagerServices.getLayer(GROUP, NAME);
+
         initAcronymsMap();
     }
 
@@ -97,13 +211,122 @@ public class S57DBComponentController
         }
     }
 
+    private void initCheckBox(String filename, ChoiceBox<String> cb) {
+        String content;
+        String[] parts;
+        try {
+            content = new String(Files.readAllBytes(Paths.get(filename)));
+            parts = content.split(",");
+            ObservableList<String> cbData = FXCollections.observableArrayList();
+            cbData.addAll(Arrays.asList(parts));
+            cb.setItems(cbData);
+            System.out.println("cbData : " + cbData);
+        } catch (IOException ex) {
+            Logger.getLogger(S57DBComponentController.class.getName()).log(Level.SEVERE, ex.toString(), ex);
+        }
+    }
+
     public final void init(String path) {
 
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        databasesCB.setItems(dbCbData);
+        databasesCB.getSelectionModel().select("s57NP5DB");
+        objectsCB.setItems(objectsCbData);
+        // initCheckBox( "properties/objects_1.csv", objectsCB);
+        objectsCB.getSelectionModel().select("ALL:All objects");
+
+        quit.setOnMouseClicked((MouseEvent event) -> {
+            component.off();
+        });
+        helpButton.setOnMouseClicked((MouseEvent event) -> {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Options");
+            alert.setHeaderText("Display S57 objects");
+            Text s = new Text(
+                    "    Choice database with scale :\n\n"
+                    + "    s57NP1DB - Overview 	< 1 : 1 500 000\n"
+                    + "    s57NP2DB - General 	1 : 350 000 à 1 : 1 500 000\n"
+                    + "    s57NP3DB - Coastal 	1 : 90 000 à 1 : 350 000\n"
+                    + "    s57NP4DB - Approach 	1 : 22 000 à 1 : 90 000\n"
+                    + "    s57NP5DB - Harbour 	1 : 4 000 à 1 : 22 000\n"
+                    + "    s57NP6DB - Berthing 	> 1 : 4 000\n\n"
+                    + "    Selection of objects to display");
+            s.setWrappingWidth(650);
+            alert.getDialogPane().setContent(s);
+            alert.show();
+        });
+        latLonButton.setOnMouseClicked((MouseEvent event) -> {
+            initSelectedZone();
+        });
     }
 
+    private void initSelectedZone() {
+        
+        Dialog dialog = new Dialog<>();
+        dialog.setTitle("Create Area");
+        dialog.setHeaderText("Please enter selected area coordinates.");
+        dialog.setResizable(false);
+
+        Label lat0Label = new Label("Northwest point latitude : ");
+        Label lon0Label = new Label("Northwest point longitude : ");
+        Label lat1Label = new Label("Southeast point latatitude : ");
+        Label lon1Label = new Label("Southeast point longitude : ");
+
+        TextField lat0TF = new TextField();
+        TextField lat1TF = new TextField();
+        TextField lon0TF = new TextField();
+        TextField lon1TF = new TextField();
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20, 35, 20, 35));
+        grid.add(lat0Label, 0, 0);
+        grid.add(lat0TF, 1, 0);
+        grid.add(lon0Label, 0, 1);
+        grid.add(lon0TF, 1, 1);
+
+        grid.add(lat1Label, 0, 2);
+        grid.add(lat1TF, 1, 2);
+        grid.add(lon1Label, 0, 3);
+        grid.add(lon1TF, 1, 3);
+
+        dialog.getDialogPane().setContent(grid);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        Button okButton = (Button) dialog.getDialogPane().lookupButton(ButtonType.OK);
+        okButton.addEventFilter(ActionEvent.ACTION, event -> {
+            try{
+            lat0 = Double.valueOf(lat0TF.getText().trim());
+            lon0 = Double.valueOf(lon0TF.getText().trim());
+            lat1 = Double.valueOf(lat1TF.getText().trim());
+            lon1 = Double.valueOf(lon1TF.getText().trim());
+            }catch(NumberFormatException e ){
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("Range latitude : -90° <= Latitude <= 90° \n"
+                +"Range longitude : -180° <= Longitude <= 180°");
+                alert.show();
+            }
+            if (lat0 < -90.0 || lat0 > 90.0 || lat1 < -90.0 || lat1 > 90.0 ) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("Range latitude : -90° <= Latitude <= 90°");
+                alert.show();
+            }
+            if (lon0 < -180.0 || lon0 > 180.0 || lon1 < -180.0 || lon1 > 180.0) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("Range longitude : -180° <= Lonitude <= 180°");
+                alert.show();
+            }
+            event.consume();
+            dialog.close();
+        });
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+        dialog.showAndWait();
+    }
 }
