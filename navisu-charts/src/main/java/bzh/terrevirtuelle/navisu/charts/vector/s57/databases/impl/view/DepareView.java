079b8bfa9f79a2245316e2b460312b3dd861350f
serge <serge@serge-XPS-8500>
2018-03-23 18:10:38 +0100

    Depare display with color : OK

--- navisu-charts/src/main/java/bzh/terrevirtuelle/navisu/charts/vector/s57/databases/impl/view/DepareView.java
+++ navisu-charts/src/main/java/bzh/terrevirtuelle/navisu/charts/vector/s57/databases/impl/view/DepareView.java
@@ -6,24 +6,10 @@
 package bzh.terrevirtuelle.navisu.charts.vector.s57.databases.impl.view;
 
 import bzh.terrevirtuelle.navisu.core.view.geoview.worldwind.impl.GeoWorldWindViewImpl;
-import bzh.terrevirtuelle.navisu.domain.charts.vector.ndf.view.NFD_COLOUR;
 import bzh.terrevirtuelle.navisu.shapefiles.impl.controller.loader.SingleAREA_ShapefileLoader;
-import bzh.terrevirtuelle.navisu.topology.TopologyServices;
 import gov.nasa.worldwind.WorldWindow;
-import gov.nasa.worldwind.avlist.AVKey;
 import gov.nasa.worldwind.formats.shapefile.Shapefile;
-import gov.nasa.worldwind.formats.shapefile.ShapefileRecord;
-import gov.nasa.worldwind.formats.shapefile.ShapefileRecordPolygon;
-import gov.nasa.worldwind.geom.Sector;
 import gov.nasa.worldwind.layers.RenderableLayer;
-import gov.nasa.worldwind.render.BasicShapeAttributes;
-import gov.nasa.worldwind.render.Material;
-import gov.nasa.worldwind.render.Polygon;
-import gov.nasa.worldwind.render.ShapeAttributes;
-import gov.nasa.worldwind.render.SurfacePolygons;
-import java.awt.Color;
-import java.util.Map;
-import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -31,21 +17,15 @@ import java.util.logging.Logger;
  *
  * @author serge
  */
-public class DepareView {
+public class DepareView
+        extends PolygonView {
 
-    protected RenderableLayer layer;
-    protected Polygon polygon;
-    protected String label;
     protected double val1;
     protected double val2;
-    protected Color color;
-
-    private ShapefileRecord record;
-    private Set<Map.Entry<String, Object>> entries;
-    private SurfacePolygons shape;
+    protected WorldWindow wwd = GeoWorldWindViewImpl.getWW();
 
     public DepareView(RenderableLayer layer) {
-        this.layer = layer;
+        super(layer);
     }
 
     public void display(Shapefile shp) {
@@ -58,10 +38,10 @@ public class DepareView {
                         entries = record.getAttributes().getEntries();
                         entries.stream().filter((e) -> (e != null)).forEachOrdered((e) -> {
                             if (e.getKey().equalsIgnoreCase("drval1")) {
-                                val1 = (Double)e.getValue();
+                                val1 = (Double) e.getValue();
                             }
                             if (e.getKey().equalsIgnoreCase("drval2")) {
-                                val2 = (Double)e.getValue();
+                                val2 = (Double) e.getValue();
                             }
                             color = defineColor(val1, val2);
                         });
@@ -72,98 +52,15 @@ public class DepareView {
 
                     createPolygon(record);
                     setPolygonAttributes(shape, color);
-                    
+
                     layer.addRenderable(shape);
-                    label = "";
+                     label = "";
+                   //  wwd.redrawNow();
                 }
             } catch (Exception ex) {
                 Logger.getLogger(SingleAREA_ShapefileLoader.class.getName()).log(Level.SEVERE, ex.toString(), ex);
             }
         }
-    }
-
-    protected void createPolygon(ShapefileRecord record) {
-        this.record = record;
-        shape = new SurfacePolygons(
-                Sector.fromDegrees(((ShapefileRecordPolygon) record).getBoundingRectangle()),
-                record.getCompoundPointBuffer());
-        shape.setWindingRule(AVKey.CLOCKWISE);
-        shape.setPolygonRingGroups(new int[]{0});
-    }
-
-    protected void setPolygonAttributes(SurfacePolygons shape, Color color) {
-
-        ShapeAttributes normalAttributes = new BasicShapeAttributes();
-        normalAttributes.setDrawInterior(true);
-        normalAttributes.setInteriorMaterial(new Material(color));
-        normalAttributes.setDrawOutline(true);
-        normalAttributes.setOutlineMaterial(new Material(Color.BLACK));
-        normalAttributes.setEnableLighting(true);
-        shape.setAttributes(normalAttributes);
-
-        ShapeAttributes highlightAttributes = new BasicShapeAttributes(normalAttributes);
-        highlightAttributes.setOutlineOpacity(1);
-        highlightAttributes.setDrawInterior(true);
-        highlightAttributes.setInteriorMaterial(new Material(Color.WHITE));
-        highlightAttributes.setInteriorOpacity(.5);
-        highlightAttributes.setEnableLighting(true);
-        shape.setHighlightAttributes(highlightAttributes);
-    }
-
-    private Color defineColor(double val1, double val2) {
-        color = new Color(159, 215, 247);
-
-        if (val1 >= -14.0 && val2 <= 0.0) {
-            color = new Color(151, 199, 0);
-            // color = new Color(87, 137, 108);
-        }
-        if (val1 >= 0.0 && val2 <= 12.0) {
-            color = new Color(91, 175, 247);
-        }
-        if (val1 >= 0.0 && val2 <= 8.0) {
-            //color = new Color(31, 175, 247);
-            color = new Color(115, 182, 239);
-        }
-        if (val1 >= 0.0 && val2 <= 3.0) {
-            color = new Color(31, 175, 247);
-        }
-        if (val1 == 5.0 && val2 <= 10.0) {
-            color = new Color(159, 215, 247);
-        }
-        if (val1 >= 5.0 && val2 <= 25.0) {//20.0
-            color = new Color(159, 215, 247);
-        }
-        if (val1 == 10.0 && val2 <= 20.0) {
-            color = new Color(247, 247, 247);
-        }
-        if (val1 == 10.0 && val2 <= 30.0) {
-            color = new Color(247, 247, 247);
-        }
-        if (val1 == 20.0 && val2 <= 30.0) {
-            color = new Color(247, 247, 247);
-        }
-        if (val1 >= 15.0 && val2 <= 50.0) {
-            color = new Color(129, 195, 226);
-        }
-        if (val1 == 30.0 && val2 <= 50.0) {
-            color = new Color(247, 247, 247);
-        }
-        if (val1 == 50.0 && val2 <= 5000.0) {
-            color = new Color(247, 247, 247);
-        }
-        if (val1 >= 20.0 && val2 <= 5000.0) {
-            color = new Color(247, 247, 247);
-        }
-        if (val2 >= 100.0) {
-            color = new Color(247, 247, 247);
-        }
-        // pour une mer bleue, en mode nav
-        /* 
-         if (val1 >= -20.0 && val2 <= 5000.0) {
-         color = new Color(10, 38, 51);
-         }
-         */
-        return color;
-
+         wwd.redrawNow();
     }
 }
--- navisu-launcher/tmp/cmd.sh
+++ navisu-launcher/tmp/cmd.sh
@@ -1,2 +1,2 @@
-/usr/bin/ogr2ogr -f "ESRI Shapefile" tmp/tmp.shp PG:"host=localhost user=admin password=admin dbname=s57NP5DB" -sql "SELECT geom,drval1,drval2 FROM depare WHERE geom && ST_MakeEnvelope(-5.080368314679189,45.76653541731316,1.0071145665621837,49.84217625663155)"
-/usr/bin/ogr2ogr -clipdst -5.080368314679189 45.76653541731316 1.0071145665621837 49.84217625663155 tmp/depare.shp tmp/tmp.shp
+/usr/bin/ogr2ogr -f "ESRI Shapefile" tmp/tmp.shp PG:"host=localhost user=admin password=admin dbname=s57NP5DB" -sql "SELECT geom,drval1,drval2 FROM depare WHERE geom && ST_MakeEnvelope(-5.0169605104730195,48.1825832198153,-4.572030314454884,48.478382377543674)"
+/usr/bin/ogr2ogr -clipdst -5.0169605104730195 48.1825832198153 -4.572030314454884 48.478382377543674 tmp/depare.shp tmp/tmp.shp
