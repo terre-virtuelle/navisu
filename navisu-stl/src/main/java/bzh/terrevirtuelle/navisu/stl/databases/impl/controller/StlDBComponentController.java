81279c655fe72321f51a31bd8fcffa1d2937cc27
serge <serge@serge-XPS-15-9550>
2019-04-29 14:26:05 +0200

    Bathy acquisition, test : OK

--- navisu-stl/src/main/java/bzh/terrevirtuelle/navisu/stl/databases/impl/controller/StlDBComponentController.java
+++ navisu-stl/src/main/java/bzh/terrevirtuelle/navisu/stl/databases/impl/controller/StlDBComponentController.java
@@ -1358,12 +1358,12 @@ public class StlDBComponentController
                     latMax + 10 * RETRIEVE_OFFSET, lonMax + 10 * RETRIEVE_OFFSET, 100, 100, 0.0);
             Point3DGeo[][] pts1 = jtsServices.mergePointsToGrid(demBathy.getGrid(), pts);
           
-             //DEBUG_0
-            //createGridFromDelaunayBathymetry(demBathy);
-            //DEBUG_0
-            //DEBUG_1
-            displayServices.displayPoints3D(demBathy.getGrid(), s57Layer);
-            //DEBUG_1
+            //TEST_0
+            // createGridFromDelaunayBathymetry(demBathy);
+            //TEST_0
+            //TEST_1
+            // displayServices.displayPoints3D(demBathy.getGrid(), s57Layer);
+            //TEST_1
 
             //Transformation en tableau lat decroissantes pour GeoTiff
             Point3DGeo[][] gridBathy0 = new Point3DGeo[pts1.length][pts1[0].length];
